0ab3c3689cc7f64ae524682b60f46b0fc0542cd4
TOTHEMOON\youdo <1023571809@qq.com>
2025-09-20 22:14:18 +0800

    项目设置

---
+++ CarMan/Assets/CarMan/MyEvent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public static class MyEvent
+{
+    public static UnityEvent LoadEvent = new UnityEvent();
+}
+
+
+
+
+
+
+// MyEvent.LoadEvent.AddListener(SetOriginAsset);
+
+// MyEvent.LoadEvent.RemoveListener(SetOriginAsset);
