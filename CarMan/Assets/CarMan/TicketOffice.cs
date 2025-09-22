c0c3469551284a653ca6df91febd9eece1c38b53
TOTHEMOON\youdo <1023571809@qq.com>
2025-09-22 19:39:03 +0800

    硬币过关

--- CarMan/Assets/CarMan/TicketOffice.cs
+++ CarMan/Assets/CarMan/TicketOffice.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TicketOffice : MonoBehaviour
 {
     public int ticketCount = 0;
+    public RodAxis rodAxis;
+    private bool hasOpened = false; // 标记是否已经触发过打开
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,14 @@ public class TicketOffice : MonoBehaviour
     {
         ticketCount++;
         Debug.LogError("Ticket count: " + ticketCount);
+        
+        // 当票数达到5且尚未触发过打开时，触发RodAxis的Open方法
+        if (ticketCount >= 5 && !hasOpened && rodAxis != null)
+        {
+            rodAxis.Open();
+            hasOpened = true;
+            Debug.LogError("Ticket count reached 5! Opening rod axis.");
+        }
     }
 
     // Update is called once per frame
