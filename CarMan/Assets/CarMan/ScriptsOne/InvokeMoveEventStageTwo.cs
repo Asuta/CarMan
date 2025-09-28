8b457496fce57a1f4201caa549369b2284a5c9c6
TOTHEMOON\youdo <1023571809@qq.com>
2025-09-28 22:23:34 +0800

    level 2 ok

--- CarMan/Assets/CarMan/ScriptsOne/InvokeMoveEventStageTwo.cs
+++ CarMan/Assets/CarMan/ScriptsOne/InvokeMoveEventStageTwo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InvokeMoveEventStageTwo : MonoBehaviour
 {
+    bool   isPressed  = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,10 @@ public class InvokeMoveEventStageTwo : MonoBehaviour
 
     public void InvokeMoveEventFunc()
     {
-        MyEvent.SystemStartEventTwo.Invoke();
+        if(isPressed)
+        {
+            MyEvent.SystemStartEventTwo.Invoke();
+            isPressed = false;
+        }
     }
 }
